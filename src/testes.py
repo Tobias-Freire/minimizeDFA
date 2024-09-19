6961cc4139ad2516d0c993a4ea8b472bb47b1734
tobiasfreire <tobiasfreire005@gmail.com>
2024-09-19 10:54:34 -0300

    Remoção de arquivo de testes

--- src/testes.py
+++
@@ -1,6 +0,0 @@
-from getAutomaton import getAutomaton
-from verifyAutomaton import verify
-
-automato = getAutomaton("./automatos/automatoF.txt")
-result = verify(automato["transicoes"])
-print(result)
