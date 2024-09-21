d716f7013032151ced887900b457194adade002f
tobiasfreire <tobiasfreire005@gmail.com>
2024-09-21 17:03:22 -0300

    Agora um arquivo com o automato minimizado é feito

--- src/AFD_NERODE_LOGIC/minimizeAutomaton.py
+++ src/AFD_NERODE_LOGIC/minimizeAutomaton.py
@@ -1,5 +1,7 @@
+import os
 from getAutomaton import getAutomaton
 from verifyAutomaton import verify
+from write_automaton import writeAutomaton
 
 def minimizeAutomaton(path_to_automaton: str) -> int:
     """
@@ -110,7 +112,13 @@ def minimizeAutomaton(path_to_automaton: str) -> int:
 
     if(automatonIsCorrect):
         print("\033[92mO autômato passado é um AFD!\033[0m\nComeçando processamento de minimização...")
-        print(nerodeAlgorithm(automato))
+        automato_mini = nerodeAlgorithm(automato)
+        print(automato_mini)
+        # Obtém o nome do arquivo de texto do automato original a partir do caminho
+        archive_original_name = os.path.basename(path_to_automaton)
+        # Remove a extensão (pois em write_automaton ele adiciona a extensão .txt)
+        archive_original_name = os.path.splitext(archive_original_name)[0]
+        writeAutomaton(automato_mini, archive_original_name, '././automatos')
         return 1
     else:
         print("\033[91mO autômato passado não é um AFD\033[0m")
---
+++ src/AFD_NERODE_LOGIC/write_automaton.py
@@ -0,0 +1,43 @@
+import os
+
+def writeAutomaton(automato: map, name: str, path: str):
+    """
+    Função que escreve o autômato definido por um mapa em um arquivo de texto
+
+    Args:
+        automaton (map): mapa que define o automato passado como argumento
+        name (str): nome do automato
+        path (str): caminho para onde o usuário quer salvar o arquivo de texto
+
+    Returns:
+        Nenhum, mas um arquivo de texto é escrito e armazenado no local especificado
+    """
+    
+    # Montar o conteúdo do arquivo
+    conteudo = []
+    
+    # Escreve o alfabeto
+    conteudo.append("alfabeto:" + ",".join(automato["alfabeto"]))
+    
+    # Escreve os estados
+    conteudo.append("estados:" + ",".join(automato["estados"]))
+    
+    # Escreve o estado inicial
+    conteudo.append("inicial:" + automato["estado_inicial"])
+    
+    # Escreve os estados finais
+    conteudo.append("finais:" + ",".join(automato["estados_finais"]))
+    
+    # Escreve as transições
+    conteudo.append("transicoes")
+    for origem, destino, simbolo in automato["transicoes"]:
+        conteudo.append(f"{origem}, {destino}, {simbolo}")
+    
+    # Define o caminho completo para salvar o arquivo
+    caminho_completo = os.path.join(path, name + "_minimized.txt")
+    
+    # Escreve o conteúdo no arquivo
+    with open(caminho_completo, "w") as arquivo:
+        arquivo.write("\n".join(conteudo))
+    
+    print(f"Autômato salvo em: {caminho_completo}")
